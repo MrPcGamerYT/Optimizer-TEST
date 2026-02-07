36aafd07672fb929eeaaf6119e42b0cb6e9306cb
MR.PC GAMER <ayanr8096@gmail.com>
2026-02-07 12:39:14 +0530

    Update AssemblyInfo.cs

--- Optimizer/Properties/AssemblyInfo.cs
+++ Optimizer/Properties/AssemblyInfo.cs
@@ -2,6 +2,9 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
 [assembly: AssemblyTitle("Optimizer")]
 [assembly: AssemblyDescription("System Optimizer Tool")]
 [assembly: AssemblyConfiguration("")]
@@ -10,8 +13,21 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("© 2026 Mr.Pc Gamer. All Rights Reserved.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: ComVisible(false)]
-[assembly: Guid("08e58f25-5dd2-42fe-833a-23ad7133b493")]
-[assembly: AssemblyVersion("1.0.1.83")]
-[assembly: AssemblyFileVersion("1.0.1.83")]
 
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("08e58f25-5dd2-42fe-833a-23ad7133b493")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.2.3")]
+[assembly: AssemblyFileVersion("1.0.2.3")]
